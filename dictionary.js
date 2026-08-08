e2c83912c95339b9cfd815424105d8c18f3d1ea6
max.villalba <maxvillalbapons@gmail.com>
2026-08-08 12:40:05 +0200

    Augment de diccionari especial x detectar barabrismes

--- dictionary.js
+++ dictionary.js
@@ -1,6 +1,163 @@
+// Diccionari de castellanismes i barbarismes habituals en català.
+// wrong: correct
+//
+// NOTA: aquest diccionari és una solució temporal per a l'MVP.
+// Quan s'incorpori un motor de detecció més avançat (IA/gramàtica),
+// aquest fitxer probablement s'eliminarà o quedarà com a llista
+// complementària de casos molt clars i inequívocs.
+//
+// Fonts de referència: CPNL (cpnl.cat/gramatica), reculls de
+// barbarismes contrastats (boncatala.com, correccioencatala.cat).
+
 const dictionary = {
-    "Aixo": "Això",
-    "provaa": "prova",
-    "cotxee": "cotxe",
-    "Barcelonaa": "Barcelona"
+
+    // A
+    abarcar: "abastar",
+    abatir: "abatre",
+    abertura: "obertura",
+    abobat: "encantat",
+    abollar: "abonyegar",
+    abort: "avortament",
+    abotxornar: "avergonyir",
+    acantilat: "penya-segat",
+    acarrejar: "ocasionar",
+    acaudalat: "adinerat",
+    acera: "vorera",
+    aclarar: "aclarir",
+    aclaracio: "aclariment",
+    acometida: "escomesa",
+    aconteixement: "esdeveniment",
+    acoplament: "acoblament",
+    acostumbrar: "acostumar",
+    acovardar: "acovardir",
+    acreedor: "creditor",
+    adelantar: "avançar",
+    ademes: "a més",
+    agobiat: "aclaparat",
+    agotar: "esgotar",
+    agravar: "agreujar",
+    agujetes: "cruiximent",
+    albedrio: "arbitri",
+    alcançar: "assolir",
+    algo: "alguna cosa",
+    ambos: "tots dos",
+    amparo: "empara",
+    apoiar: "recolzar",
+    armonia: "harmonia",
+    arrepentirse: "penedir-se",
+    asamblea: "assemblea",
+    atras: "endarreriment",
+    atravessar: "travessar",
+
+    // B
+    baraja: "baralla",
+    barato: "barat",
+    barrer: "escombrar",
+    bisagra: "frontissa",
+    borde: "tosc",
+    borrar: "esborrar",
+    bassura: "escombraries",
+    busqueda: "cerca",
+
+    // C
+    cadera: "maluc",
+    calcetin: "mitjó",
+    cansancio: "cansament",
+    carinyo: "afecte",
+    cerrar: "tancar",
+    cerradura: "pany",
+    cocher: "cotxer",
+    conejo: "conill",
+    conserje: "conserge",
+    corriente: "corrent",
+
+    // D
+    demasiado: "massa",
+    demes: "resta",
+    desarrollo: "desenvolupament",
+    despacio: "a poc a poc",
+    disfrutar: "gaudir",
+
+    // E
+    embarazada: "embarassada",
+    empeño: "empenyorament",
+    encender: "encendre",
+    enterar: "assabentar",
+    entonces: "aleshores",
+    esconder: "amagar",
+    escuchar: "escoltar",
+    esposas: "manilles",
+    estropear: "espatllar",
+
+    // F
+    fecha: "data",
+    finiquito: "quitança",
+    flecha: "fletxa",
+    fregona: "baieta",
+
+    // G
+    gafas: "ulleres",
+    gasto: "despesa",
+    grifo: "aixeta",
+
+    // H
+    hueco: "buit",
+
+    // I
+    igual: "potser",
+
+    // J
+    jefe: "cap",
+    juguete: "joguina",
+
+    // L
+    limpiar: "netejar",
+    listo: "llest",
+
+    // LL / M
+    llavero: "clauer",
+    madrugada: "matinada",
+    maletero: "portaequipatges",
+    medir: "mesurar",
+    mentira: "mentida",
+    mientras: "mentre",
+
+    // O
+    olvidar: "oblidar",
+
+    // P
+    parrafo: "paràgraf",
+    pasillo: "passadís",
+    pegar: "enganxar",
+    pelea: "baralla",
+    plazo: "termini",
+    porvenir: "futur",
+    puesto: "lloc",
+
+    // Q
+    quiza: "potser",
+
+    // R
+    rato: "estona",
+    raro: "estrany",
+    recibo: "rebut",
+    relleno: "farcit",
+    repente: "de sobte",
+
+    // S
+    serio: "seriós",
+    suceso: "succés",
+    suelo: "terra",
+
+    // T
+    tampoco: "tampoc",
+    tarea: "tasca",
+    tijeras: "tisores",
+    todavia: "encara",
+    tonteria: "ximpleria",
+    trozo: "tros",
+
+    // V
+    varios: "diversos",
+
 };
